ff33c6283be118eeea97d985eb04b4b93ce4af2d
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-03-10 14:21:41 +0200

    fix small error and add 2 demo cases

--- test/phoneRegister.test.js
+++ test/phoneRegister.test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 
 const PhoneRegister = require('../phoneRegister');
-const phones = require('..phones.json');
+const phones = require('../phones.json');
 
 describe('test get types', function () {
     it('using default data', function () {
@@ -20,4 +20,12 @@ describe('test get types', function () {
         const register = new PhoneRegister([]);
         expect(register.getTypes()).to.deep.equal([]);
     });
-})
+    it('demo equal', function () { // true because refers to same array
+        const data = [];
+        expect(data).to.equal(data);
+    });
+    it('demo deep equal', function () { // fails with equal, because it refers to different arrays
+        const data = [];
+        expect(data).to.deep.equal([]);
+    });
+});
